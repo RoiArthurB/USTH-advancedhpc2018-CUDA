ad63f623dcbe48177c001a4ebbe1e798223c1fd1
Arthur Brugiere <arbr1@hotmail.fr>
2018-11-18 17:04:16 +0700

    [FIX] Failed merge src/labwork.cu
    J'avais pas vu LOL

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -115,7 +115,19 @@ void Labwork::labwork1_CPU() {
 }
 
 void Labwork::labwork1_OpenMP() {
+    int pixelCount = inputImage->width * inputImage->height;
+    outputImage = static_cast<char *>(malloc(pixelCount * 3));
 
+    #pragma omp parallel for
+    for (int j = 0; j < 100; j++) {     // let's do it 100 times, otherwise it's too fast!
+        #pragma omp parallel for
+        for (int i = 0; i < pixelCount; i++) {
+            outputImage[i * 3] = (char) (((int) inputImage->buffer[i * 3] + (int) inputImage->buffer[i * 3 + 1] +
+                                          (int) inputImage->buffer[i * 3 + 2]) / 3);
+            outputImage[i * 3 + 1] = outputImage[i * 3];
+            outputImage[i * 3 + 2] = outputImage[i * 3];
+        }
+    }
 }
 
 int getSPcores(cudaDeviceProp devProp) {
@@ -144,16 +156,126 @@ int getSPcores(cudaDeviceProp devProp) {
     return cores;
 }
 
+//Get to know your GPU
 void Labwork::labwork2_GPU() {
-    
+    int nDevices = 0;
+    // get all devices
+    cudaGetDeviceCount(&nDevices);
+    printf("Number total of GPU : %d\n\n", nDevices);
+    for (int i = 0; i < nDevices; i++){
+        //get informations from individual device
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+
+        //Device info
+        //=================
+        printf("Device Number : %d\n", i);
+        printf("Device Name : %s\n", prop.name);
+
+        //Core info
+        //=================
+        //clock rate
+        printf("\tClock Rate (KHz): %f\n", prop.clockRate);
+        //core counts
+        printf("\tCore Number : %d\n", getSPcores(prop));
+        //multiprocessor count
+        printf("\tMultiprocessor Number : %d\n", prop.multiProcessorCount);
+        //warp size
+        printf("\tWarp Size : %d\n", prop.warpSize);
+
+        //Memory info
+        //=================
+        //clock rate
+        printf("\tMemory Clock Rate (KHz): %f\n", prop.memoryClockRate);
+        //bus width 
+        printf("\tMemory Bus Width (bits): %d\n", prop.memoryBusWidth);
+        //[optional] bandwidth
+        printf("\tPeak Memory Bandwidth (GB/s): %f\n\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    }
+
 }
 
+__global__ void grayscale(uchar3 *input, uchar3 *output) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    output[tid].x = (input[tid].x + input[tid].y + input[tid].z) / 3;
+    output[tid].z = output[tid].y = output[tid].x;
+}
+//Do labwork1 with CUDA
 void Labwork::labwork3_GPU() {
-   
+    // Preparing var
+    //======================
+    //Calculate number of pixels
+    int pixelCount = inputImage->width * inputImage->height;
+    outputImage = static_cast<char *>(malloc(pixelCount * 3));
+    uchar3 *devInput;
+    uchar3 *devGray;
+
+    //Allocate CUDA memory    
+    cudaMalloc(&devInput, pixelCount * sizeof(uchar3));
+    cudaMalloc(&devGray, pixelCount * sizeof(uchar3));
+    // Copy CUDA Memory from CPU to GPU
+    cudaMemcpy(devInput, inputImage->buffer, pixelCount * sizeof(uchar3), cudaMemcpyHostToDevice);
+
+    // Processing
+    //======================
+    // Start GPU processing (KERNEL)
+    int blockSize = 1024;
+    int numBlock = pixelCount / blockSize;
+    grayscale<<<numBlock, blockSize>>>(devInput, devGray);
+    // Copy CUDA Memory from GPU to CPU
+    cudaMemcpy(outputImage, devGray, pixelCount * sizeof(uchar3), cudaMemcpyDeviceToHost);
+
+    // Cleaning
+    //======================
+    // Free CUDA Memory
+    cudaFree(&devInput);
+    cudaFree(&devGray);
 }
 
+__global__ void grayscale2D(uchar3 *input, uchar3 *output, int imgWidth, int imgHeight) {
+    //Calculate tid
+    int tidx = threadIdx.x + blockIdx.x * blockDim.x;
+    int tidy = threadIdx.y + blockIdx.y * blockDim.y;
+    if (tidx >= imgWidth || tidy >= imgHeight) return;
+
+    int tid =  tidx + (tidy * imgWidth);
+
+    //Process pixel
+    output[tid].x = (input[tid].x + input[tid].y + input[tid].z) / 3;
+    output[tid].z = output[tid].y = output[tid].x;
+}
+//Improve labwork 3 code to use 2D blocks
 void Labwork::labwork4_GPU() {
-   
+    // Preparing var
+    //======================
+    //Calculate number of pixels
+    int pixelCount = inputImage->width * inputImage->height;
+    outputImage = static_cast<char *>(malloc(pixelCount * 3));
+    uchar3 *devInput;
+    uchar3 *devGray;
+
+    //Allocate CUDA memory    
+    cudaMalloc(&devInput, pixelCount * sizeof(uchar3));
+    cudaMalloc(&devGray, pixelCount * sizeof(uchar3));
+    // Copy CUDA Memory from CPU to GPU
+    cudaMemcpy(devInput, inputImage->buffer, pixelCount * sizeof(uchar3), cudaMemcpyHostToDevice);
+
+    // Processing
+    //======================
+    // Start GPU processing (KERNEL)
+    //Create 32x32 Blocks
+    dim3 blockSize = dim3(32, 32);
+    dim3 gridSize = dim3((inputImage->width + (blockSize.x-1))/blockSize.x, 
+        (inputImage->height  + (blockSize.y-1))/blockSize.y);
+    grayscale2D<<<gridSize, blockSize>>>(devInput, devGray, inputImage->width, inputImage->height);
+    // Copy CUDA Memory from GPU to CPU
+    cudaMemcpy(outputImage, devGray, pixelCount * sizeof(uchar3), cudaMemcpyDeviceToHost);
+
+    // Cleaning
+    //======================
+    // Free CUDA Memory
+    cudaFree(&devInput);
+    cudaFree(&devGray);
 }
 
 // CPU implementation of Gaussian Blur
@@ -193,8 +315,149 @@ void Labwork::labwork5_CPU() {
     }
 }
 
-void Labwork::labwork5_GPU() {
-    
+__global__ void GaussianBlur(uchar3 *input, uchar3 *output, int imgWidth, int imgHeight){
+
+
+    int kernel[] = { 0, 0, 1, 2, 1, 0, 0,  
+                     0, 3, 13, 22, 13, 3, 0,  
+                     1, 13, 59, 97, 59, 13, 1,  
+                     2, 22, 97, 159, 97, 22, 2,  
+                     1, 13, 59, 97, 59, 13, 1,  
+                     0, 3, 13, 22, 13, 3, 0,
+                     0, 0, 1, 2, 1, 0, 0 };
+
+    //Calculate tid
+    int tidx = threadIdx.x + blockIdx.x * blockDim.x;
+    int tidy = threadIdx.y + blockIdx.y * blockDim.y;
+    if (tidx >= imgWidth || tidy >= imgHeight) return;
+
+    int sum = 0;
+    int c = 0;
+    for (int y = -3; y <= 3; y++) {
+        for (int x = -3; x <= 3; x++) {
+            int i = tidx + x;
+            int j = tidy + y;
+            
+            //We won't take pixel outside of the image.
+            if (i < 0) continue;
+            if (i >= imgWidth) continue;
+            if (j < 0) continue;
+            if (j >= imgHeight) continue;
+            
+            int tid = imgWidth * j + i; // RowSize * j + i, get the position of our pixel
+            
+            //Applying gray filter
+            unsigned char gray = (input[tid].x + input[tid].y + input[tid].z) / 3;
+            
+            //Applying Gaussian blur and stuff
+            int coefficient = kernel[(y+3) * 7 + x + 3];
+            sum = sum + gray * coefficient;
+            c += coefficient;
+        }
+    }
+
+    sum /= c;
+    int posOut = tidx + tidy * imgWidth;
+    output[posOut].y = output[posOut].x = output[posOut].z = sum;
+
+}
+__global__ void sharedGaussianBlur(uchar3 *input, uchar3 *output, int imgWidth, int imgHeight, int *kernelMatrix){
+
+    int tidx = threadIdx.x + blockIdx.x * blockDim.x;
+    int tidy = threadIdx.y + blockIdx.y * blockDim.y;
+    if (tidx >= imgWidth || tidy >= imgHeight) return;
+
+    // Get thread's index number inside the block
+    int tid = threadIdx.x + threadIdx.y * blockDim.x;
+
+    __shared__ int sharedKernel[49]; 
+
+    // Fill shared array
+    if (tid < 49)
+        sharedKernel[tid] = kernelMatrix[tid];
+
+    // Wait shared array to be completed
+    __syncthreads();    
+
+    // Core function
+    //======================
+    int sum = 0;
+    int c = 0;
+    for (int y = -3; y <= 3; y++) {
+        for (int x = -3; x <= 3; x++) {
+            int i = tidx + x;
+            int j = tidy + y;
+            
+            //We won't take pixel outside of the image.
+            if (i < 0) continue;
+            if (i >= imgWidth) continue;
+            if (j < 0) continue;
+            if (j >= imgHeight) continue;
+            
+            tid = imgWidth * j + i; // RowSize * j + i, get the position of our pixel
+            
+            //Applying gray filter
+            unsigned char gray = (input[tid].x + input[tid].y + input[tid].z) / 3;
+            
+            //Applying Gaussian blur and stuff
+            int coefficient = sharedKernel[(y+3) * 7 + x + 3];
+            sum = sum + gray * coefficient;
+            c += coefficient;
+        }
+    }
+
+    sum /= c;
+    int posOut = tidx + tidy * imgWidth;
+    output[posOut].y = output[posOut].x = output[posOut].z = sum;
+
+}
+void Labwork::labwork5_GPU(bool shared) {
+    // var
+    //======================
+    int kernel[] = { 0, 0, 1, 2, 1, 0, 0,  
+                     0, 3, 13, 22, 13, 3, 0,  
+                     1, 13, 59, 97, 59, 13, 1,  
+                     2, 22, 97, 159, 97, 22, 2,  
+                     1, 13, 59, 97, 59, 13, 1,  
+                     0, 3, 13, 22, 13, 3, 0,
+                     0, 0, 1, 2, 1, 0, 0 };
+
+    int pixelCount = inputImage->width * inputImage->height;
+    outputImage = (char*) malloc(pixelCount * sizeof(char) * 3);
+
+    // GPU Var
+    uchar3 *devInput;
+    uchar3 *devGray;
+    int *gpuKernel;
+    //Allocate CUDA memory    
+    cudaMalloc(&devInput, pixelCount * sizeof(uchar3));
+    cudaMalloc(&devGray, pixelCount * sizeof(uchar3));
+    cudaMalloc(&gpuKernel, sizeof(kernel));
+    // Copy CUDA Memory from CPU to GPU
+    cudaMemcpy(devInput, inputImage->buffer, pixelCount * sizeof(uchar3), cudaMemcpyHostToDevice);
+    cudaMemcpy(gpuKernel, kernel, sizeof(kernel), cudaMemcpyHostToDevice);
+
+    // Processing
+    //======================
+    // Start GPU processing (KERNEL)
+    //Create 32x32 Blocks
+    dim3 blockSize = dim3(32, 32);
+    dim3 gridSize = dim3((inputImage->width + (blockSize.x-1))/blockSize.x, 
+        (inputImage->height  + (blockSize.y-1))/blockSize.y);
+    if (shared){
+        sharedGaussianBlur<<<gridSize, blockSize>>>(devInput, devGray, inputImage->width, inputImage->height, gpuKernel);
+    } else {
+        GaussianBlur<<<gridSize, blockSize>>>(devInput, devGray, inputImage->width, inputImage->height);
+    }
+    // Copy CUDA Memory from GPU to CPU
+    cudaMemcpy(outputImage, devGray, pixelCount * sizeof(uchar3), cudaMemcpyDeviceToHost);
+
+    // Cleaning
+    //======================
+    // Free CUDA Memory
+    cudaFree(&devInput);
+    cudaFree(&devGray);
+    cudaFree(&gpuKernel);
 }
 
 //labwork6a
